f388a05f323a4067bed77338ce6880a56440940b
shutupfaezan <faezanmakani13@gmail.com>
2022-11-29 00:01:33 +0530

    new initial footer

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,6 +3,7 @@ import Header from '../common/Header'
 import UserBar from '../common/UserBar'
 import "../css/HomePage.css"
 import Featured from './Featured'
+import FooterUniversal from './FooterUniversal'
 
 export default function HomePage() {
   return (
@@ -10,6 +11,7 @@ export default function HomePage() {
     <Header></Header>
     <UserBar></UserBar>
     <Featured></Featured>
+    <FooterUniversal></FooterUniversal>
     </>
   )
 }
