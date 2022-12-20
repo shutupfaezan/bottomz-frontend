0e1ff5888bb1745811ec6aeb770bce270a006db3
shutupfaezan <110533743+shutupfaezan@users.noreply.github.com>
2022-12-20 18:41:54 +0530

    header changes intro line

--- src/common/Header.js
+++ src/common/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
   return (
     <>
     
-  <div className="d-flex align-items-center" style={{background: `url('${process.env.PUBLIC_URL}/images/wave-haikei.png') no-repeat center/cover`, height: "200px" }}>
+  <div className="d-flex align-items-center" style={{background: `url('${process.env.PUBLIC_URL}/images/wave-haikei.png') no-repeat center/cover`, height: "250px" }}>
     <nav className="navbar navbar-expand navbar-light align-items-center headerback position-absolute w-100" style={{top: "0px"}}>
       <a className="navbar-brand text-white" style={{fontWeight: "800"}} href="/">BottmzUp.com</a>
       {/* <img src={process.env.PUBLIC_URL + "/images/doers-solutions-removebg-preview.png"} width="120" height="40" alt=""/> */}
@@ -34,11 +34,14 @@ export default function Header() {
         <i className="bi bi-person-circle pt-2 ml-auto ml-md-0" onClick={()=>{navigate('/stranger-login')}}style={{fontSize: "25px"}}></i>
       </div>
     </nav>
+    <div className='d-flex flex-column w-100'>
+    <h5 className='d-flex justify-content-center text-white w-75 align-self-center text-center'>Lets give you a night to remember</h5>    
     <div className='w-100'>
       <form className="d-flex justify-content-center" role="search">
-          <input className="form-control w-75" type="search" style={{height: "50px", border: "1px solid black"}} placeholder="Search Your Mind..." aria-label="Search"/>
+          <input className="form-control w-75" type="search" style={{height: "50px", border: "1px solid black"}} placeholder="Search Any Event, Club or Area.." aria-label="Search"/>
           <i className="bi bi-search position-relative" style={{float: "right", right: "30px", borderRadius: "20px", top: "15px", width: "0px"}}></i>   
       </form> 
+    </div>
     </div>
   </div>
     </>
