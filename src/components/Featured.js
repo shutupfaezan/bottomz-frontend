1e3324d047070cf0fb3aef58b6c18de7b2cbd440
shutupfaezan <110533743+shutupfaezan@users.noreply.github.com>
2022-12-21 23:15:58 +0530

    hp bug fix

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -56,7 +56,7 @@ export default function ClubsRow() {
         </div>
       </div>
       <div className='col-lg-4'>
-        <div className='mt-md-4 d-flex align-items-baseline'><h4 className='mt-2 mb-0 mx-2'><strong>Featured Events🔥</strong></h4><Link className='ml-auto d-block text-decoration-none d-lg-none' to="/all-events" style={{color: "#9E0A0A"}}>View Events</Link></div>
+        <div className='mt-md-4 d-flex align-items-baseline'><h4 className='mt-2 mb-3 mx-2'><strong>Featured Events🔥</strong></h4><Link className='ml-auto d-block text-decoration-none d-lg-none' to="/all-events" style={{color: "#9E0A0A"}}>View Events</Link></div>
         <div className='d-flex d-md-block flex-column align-items-center'>{recentEvents?.data.map((fields, index )=> {
           return <HPEvents className="col shadow-lg" key={index} identity={fields}></HPEvents>
         })}
