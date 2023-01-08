fa4c7a616a8a8a457df6d465b4f79feecb80377f
shutupfaezan <110533743+shutupfaezan@users.noreply.github.com>
2023-01-08 11:52:49 +0530

    type font and hr changes

--- src/components/HPClubs.js
+++ src/components/HPClubs.js
@@ -13,7 +13,7 @@ export default function HPClubs(props) {
       <img src={props.identity.images_url}  className="card-img-top p-2" style={{borderRadius: "22px", height: "163px"}} alt="..."/>
       <div className="p-2">
         <div className='d-flex'>
-          <h5 className="card-title m-0"><strong>{props.identity.name}</strong></h5>
+          <h5 className="card-title m-0">{props.identity.name}</h5>
           <i className=" ml-auto bi bi-heart"></i>
         </div>
         <p className='m-0' style={{fontSize: "13px"}}>{props.identity.area}</p>
--- src/components/HPEvents.js
+++ src/components/HPEvents.js
@@ -8,10 +8,10 @@ export default function HPEvents(props) {
     <img className="align-self-center mr-2" style={{width: "70px", height: "70px", borderRadius: "14px"}} src={props.identity.images_url} alt="Club"/>
     <div className="d-flex flex-column">
       <div className='d-flex'>
-      <p className="m-0 mr-2" style={{color: "#980098"}}><strong>{props.identity.date}</strong></p>
+      <p className="m-0 mr-2" style={{color: "#980098"}}>{props.identity.date}</p>
       <p className="m-0" style={{color: "gray"}}>{props.identity.day.slice(0,3)} • {props.identity.timings}</p>
       </div>
-      <p className="m-0"><strong>{props.identity.event_name}</strong></p>
+      <p className="m-0">{props.identity.event_name}</p>
       <p className='m-0' style={{fontSize: "12px", color: "grey"}}>{props.identity.event_venue}</p>
     </div>
     <div className='d-flex ml-auto'><i className="bi bi-three-dots-vertical" style={{fontSize: "30px", color: "gray"}}></i></div>
