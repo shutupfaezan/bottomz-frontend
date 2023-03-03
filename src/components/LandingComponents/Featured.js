ee632c1a3c75a7c3c5bb51e79524723f2baf2692
shutupfaezan <110533743+shutupfaezan@users.noreply.github.com>
2023-03-04 01:37:24 +0530

    changed featured page-recreated singular club page

--- src/components/LandingComponents/Featured.js
+++ src/components/LandingComponents/Featured.js
@@ -36,8 +36,8 @@ export default function ClubsRow() {
 
      }, []);
 
-     const reverseClubs = recentClubs?.data.reverse().slice(0,3)
-     const reverseEvents = recentEvents?.data.reverse().slice(0,4)
+     const reverseClubs = recentClubs?.data.reverse().slice(0,4)
+     const reverseEvents = recentEvents?.data.slice(0,6)
 
   return (
     <>
@@ -50,19 +50,19 @@ export default function ClubsRow() {
       </div>}
       {!loading && 
       <div className="my-2 px-0">
-        <section className='px-lg-5 py-3'>
-          <div className='mt-md-4 d-flex align-items-baseline mb-2 container-xl flex-column px-lg-5 px-2'>
+        <section className='px-md-5 py-3'>
+          <div className='mt-md-4 d-flex align-items-baseline mb-2 flex-column px-lg-5 px-2'>
             <h4 className='mt-2 mx-3 primary-header mb-0' style={{fontSize: "40px", color: "transparent", WebkitTextStroke: "1px black"}}>Browse By</h4>
             <h4 className="primary-header mb-3 mt-2 mx-3" style={{fontSize: "40px"}}>Upcoming Events</h4></div>
-          <div className='d-flex container-xl flex-wrap align-items-center px-md-5 p-0'>{reverseEvents?.map((fields, index )=> {
-            return <HPEvents className="" key={index} index={index} identity={fields}></HPEvents>
+          <div className='d-flex flex-wrap align-items-center px-lg-5 p-2'>{reverseEvents?.map((fields, index )=> {
+            return <HPEvents className="w-100" key={index} index={index} identity={fields}></HPEvents>
           })}
           </div>
           <Link to="/all-events" className='mt-3 d-flex justify-content-center' style={{color: "blueviolet"}}>See All<i class="bi bi-arrow-right ml-2"></i></Link>
         </section>
-        <section className='px-lg-5 pt-lg-5 pb-lg-4 px-4 py-5' style={{background: "#01040D"}}>
-          <div className=' d-flex align-items-baseline mb-2 container-xl p-0 flex-column px-md-5 ml-md-0 ml-3'><h4 className='mt-2 primary-header' style={{fontSize: "40px", color: "transparent", WebkitTextStroke: "1px white"}}>Browse Events</h4><h4 className="primary-header mb-3" style={{fontSize: "40px", color: "white"}}> By Clubs</h4></div>
-          <div className='d-flex col col-lg flex-wrap px-md-4 mx-md-3'>
+        <section className='px-lg-5 pt-lg-5 pb-lg-4 px-4 py-5 bg-light'>
+          <div className=' d-flex align-items-baseline mb-2 container-xl p-0 flex-column px-md-5 ml-md-0 ml-3'><h4 className='mt-2 primary-header' style={{fontSize: "40px", color: "transparent", WebkitTextStroke: "1px black"}}>Browse Events</h4><h4 className="primary-header mb-3" style={{fontSize: "40px", color: "black"}}> By Clubs</h4></div>
+          <div className='d-flex col p-0 flex-wrap px-md-4 mx-md-3'>
             {reverseClubs?.map((fields, index )=> {
               return <HPClubs key={index} identity={fields}></HPClubs>
             })}
--- src/components/UserComponents/AllClubs.js
+++ src/components/UserComponents/AllClubs.js
@@ -5,8 +5,10 @@ import RenderClubs from '../UserComponents/RenderClubs';
 import "../../css/AllClubs.css"
 import "../../css/ClubsRow.css"
 import GlobalHeader from '../../common/GlobalHeader'
+import { BrowserView, MobileOnlyView, TabletView } from 'react-device-detect';
+import Footer from '../../common/Footer';
 
-export default function AllClubs(props) {
+export default function AllClubs() {
     const [loading, setLoading] = useState(true)
     const [searchTerm, setSearhTerm] = useState()
     const [recentClubs, setRecentClubs] = useState()
@@ -24,8 +26,8 @@ export default function AllClubs(props) {
     }, []);
   return (
     <>
+    <div>
     <GlobalHeader/>
-    <div className='d-flex justify-content-center' style={{marginTop: "69px"}}><h1><strong>All Clubs</strong></h1></div>
     {loading && <div className='d-flex justify-content-center mt-auto'>
     <div className='d-flex align-items-center'>
     <span style={{fontSize: "35px"}}>Loading </span>
@@ -33,15 +35,55 @@ export default function AllClubs(props) {
     </div>
     </div>
     </div>}
-    {!loading && <><div className='d-flex col-lg-4 col-md-6 p-0'>
-          <form className="d-flex mt-3 mb-3 w-100 ml-lg-5 ml-md-4 mx-4" role="search">
-            <input className="form-control" type="search" style={{borderRadius: "20px", border: "0.5px solid black"}} onChange={(event)=>setSearhTerm(event.target.value)} placeholder="Search Club Name" aria-label="Search"/>
-            <i className="bi bi-search position-relative" style={{float: "right", right: "30px", borderRadius: "20px", top: "7px", width: "0px"}}></i>
-          </form>
-        </div>
-        <div className='d-flex px-3 px-md-2 flex-wrap ml-xl-4'>
+    {!loading && <>
+    <BrowserView>
+    <div className='svgbg mb-5' style={{height: "200px"}}></div>
+    <div className='d-md-flex px-5 justify-content-between position-absolute w-100' style={{top: '125px'}}>
+      <div className='d-flex'>
+        <div className='primary-header ml-5' style={{color: "transparent", WebkitTextStroke: "1px white", fontSize: "40px"}}>All</div>
+        <div className='primary-header ml-2 text-white' style={{fontSize: "40px"}}>Clubs</div>
+      </div>
+      <div className='d-flex col-lg-4 col-md-6 p-0'>
+        <form className="d-flex mt-3 mb-3 w-100 ml-lg-5 ml-md-4 mx-md-4" role="search">
+          <input className="form-control py-3" type="search" style={{borderRadius: "20px", fontSize: "12px", border: "0.5px solid black", paddingLeft: "35px"}} onChange={(event)=>setSearhTerm(event.target.value)} placeholder="Search for ..." aria-label="Search"/>
+          <i className="bi bi-search position-relative" style={{float: "left", right: "95%", borderRadius: "20px", top: "7px", width: "0px", fontSize: "13px"}}></i>
+        </form>
+      </div>
+    </div>
+    </BrowserView>
+    <TabletView>
+    <div className='svgbg mb-2' style={{height: "200px"}}></div>
+    <div className='d-md-flex pr-5 pl-5 justify-content-between position-absolute w-100' style={{top: '125px'}}>
+      <div className='d-flex'>
+        <div className='primary-header ml-2' style={{color: "transparent", WebkitTextStroke: "1px white", fontSize: "40px"}}>All</div>
+        <div className='primary-header ml-2 text-white' style={{fontSize: "40px"}}>Clubs</div>
+      </div>
+      <div className='d-flex col-lg-4 col-md-6 p-0'>
+        <form className="d-flex mt-3 mb-3 w-100 ml-lg-5 ml-md-4 mx-md-4" role="search">
+          <input className="form-control py-3" type="search" style={{borderRadius: "20px", fontSize: "12px", border: "0.5px solid black", paddingLeft: "35px"}} onChange={(event)=>setSearhTerm(event.target.value)} placeholder="Search for ..." aria-label="Search"/>
+          <i className="bi bi-search position-relative" style={{float: "left", right: "95%", borderRadius: "20px", top: "7px", width: "0px", fontSize: "13px"}}></i>
+        </form>
+      </div>
+    </div>
+    </TabletView>
+    <MobileOnlyView>
+    <div className='svgbg mb-3' style={{height: "300px"}}></div>
+    <div className='d-md-flex pr-5 pl-5 justify-content-between position-absolute w-100' style={{top: '135px'}}>
+      <div className='d-flex justify-content-center'>
+        <div className='primary-header ml-2' style={{color: "transparent", WebkitTextStroke: "1px white", fontSize: "40px"}}>All</div>
+        <div className='primary-header ml-2 text-white' style={{fontSize: "40px"}}>Clubs</div>
+      </div>
+      <div className='d-flex col-lg-4 col-md-6 p-0'>
+        <form className="d-flex mt-3 mb-3 w-100 ml-lg-5 ml-md-4 mx-md-4" role="search">
+          <input className="form-control py-3" type="search" style={{borderRadius: "20px", fontSize: "12px", border: "0.5px solid black", paddingLeft: "35px"}} onChange={(event)=>setSearhTerm(event.target.value)} placeholder="Search for ..." aria-label="Search"/>
+          <i className="bi bi-search position-relative" style={{float: "left", right: "95%", borderRadius: "20px", top: "7px", width: "0px", fontSize: "13px"}}></i>
+        </form>
+      </div>
+    </div>
+    </MobileOnlyView>
+        <div className='d-flex px-3 px-md-2 flex-wrap m-xl-5'>
         {/* eslint-disable-next-line */}
-        {recentClubs?.data?.filter((val)=>{
+        {recentClubs?.data?.reverse().filter((val)=>{
             if(searchTerm === undefined){
               return val
             }
@@ -55,8 +97,11 @@ export default function AllClubs(props) {
           return <RenderClubs key={index} identity={fields}></RenderClubs>
         })}
     </div>
-    </>
-}
+
+    <Footer/>
+    </> 
+      }
+    </div>
     </>
   )
 }
