29a1715aa305d75f2e0d35913118848cb29d207a
shutupfaezan <110533743+shutupfaezan@users.noreply.github.com>
2023-05-26 12:27:38 +0530

    hash to browser router

--- src/common/PromoterSidebar.js
+++ src/common/PromoterSidebar.js
@@ -17,10 +17,10 @@ export default function PromoterSidebar() {
           <div className='p-2 p-md-3 p-lg-0'>
           <small className='nav-linkmt-0 pl-1 pb-0 mt-lg-3' style={{color: "black", fontSize: "14px"}}>Events</small>
           <div className='d-flex d-lg-block w-100'>
-          <Link to="/host-an-event" className="nav-link my-2 mb-md-3 mr-2 p-3 d-flex flex-column d-lg-block col col-lg-11 mr-md-3" style={{color: window.location.hash === "#/host-an-event" ? "white": "black", border: "2px solid black", borderRadius: "10px", background : window.location.hash === "#/host-an-event" ? "black" : "white", boxShadow:  window.location.hash === "#/host-an-event" ? "5px 5px #E04949": null}} aria-current="page">
+          <Link to="/host-an-event" className="nav-link my-2 mb-md-3 mr-2 p-3 d-flex flex-column d-lg-block col col-lg-11 mr-md-3" style={{color: window.location.pathname === "/host-an-event" ? "white": "black", border: "2px solid black", borderRadius: "10px", background : window.location.pathname === "/host-an-event" ? "black" : "white", boxShadow:  window.location.pathname === "/host-an-event" ? "5px 5px #E04949": null}} aria-current="page">
           <i className="bi bi-collection-play mr-2"></i>Host With Us
           </Link>
-          <Link to="/promoter-events" className="nav-link my-2 mb-md-3 mr-2 p-3 d-flex flex-column d-lg-block col col-lg-11"style={{color: window.location.hash === "#/promoter-events" ? "white": "black", border: "2px solid black", borderRadius: "10px", background : window.location.hash === "#/promoter-events" ? "black" : "white", boxShadow:  window.location.hash === "#/promoter-events" ? "5px 5px #E04949": null}}>
+          <Link to="/promoter-events" className="nav-link my-2 mb-md-3 mr-2 p-3 d-flex flex-column d-lg-block col col-lg-11"style={{color: window.location.pathname === "/promoter-events" ? "white": "black", border: "2px solid black", borderRadius: "10px", background : window.location.pathname === "/promoter-events" ? "black" : "white", boxShadow:  window.location.pathname === "/promoter-events" ? "5px 5px #E04949": null}}>
             <i className="bi bi-columns-gap mr-2"></i><span>All Events</span>
           </Link>
           </div>
@@ -43,7 +43,7 @@ export default function PromoterSidebar() {
         <div className='p-2 p-md-3 p-lg-0'>
         <small className='nav-linkmt-0 pl-1 pb-0 mt-lg-3' style={{color: "black", fontSize: "14px"}}>Utility</small>
         <div className='d-flex d-lg-block w-100'>
-        <Link to="/qr-scanner" className="nav-link my-2 mb-md-3 p-3 mr-2 d-flex flex-column d-lg-block col-6 col-lg-11 mr-md-3" aria-current="page" style={{color: window.location.hash === "#/qr-scanner" ? "white": "black", border: "2px solid black", borderRadius: "10px", background : window.location.hash === "#/qr-scanner" ? "black" : "white", boxShadow: window.location.hash === "#/qr-scanner" ? "5px 5px #E04949": null}}>
+        <Link to="/qr-scanner" className="nav-link my-2 mb-md-3 p-3 mr-2 d-flex flex-column d-lg-block col-6 col-lg-11 mr-md-3" aria-current="page" style={{color: window.location.pathname === "/qr-scanner" ? "white": "black", border: "2px solid black", borderRadius: "10px", background : window.location.pathname === "/qr-scanner" ? "black" : "white", boxShadow: window.location.pathname === "/qr-scanner" ? "5px 5px #E04949": null}}>
         <i className="fa-solid fa-qrcode mb-2 mr-2"></i>QR Scanner
         </Link>
           </div>
