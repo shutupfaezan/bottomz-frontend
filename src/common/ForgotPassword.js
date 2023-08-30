6a152df42328a6d04479392395202dd5428770ef
shutup.faezan <faezanmakani13@gmail.com>
2023-08-30 13:13:10 +0530

    new stage in forgoiot password with a bug + no integration

--- src/common/ForgotPassword.js
+++ src/common/ForgotPassword.js
@@ -104,7 +104,8 @@ export default function ForgotPassword() {
                 <button
                   type="submit"
                   className="btn mt-5 py-3 col-lg-6 d-flex mx-auto justify-content-center"
-                  style={{ borderRadius: "100px", background: "white", color: "black", fontWeight: "600", fontSize: "17px"}}>
+                  style={{ borderRadius: "100px", background: "white", color: "black", fontWeight: "600", fontSize: "17px"}}
+                  onClick={() => setStep((i) => i + 1)}>
                   Verify
                 </button>
                 <div className="text-white mt-4 text-center">
@@ -119,7 +120,42 @@ export default function ForgotPassword() {
             )}
 
             {/* Stage 3*/}
-
+            {
+              step === 2 && (
+                <>
+                <div
+                  className="d-flex mx-auto rounded-circle my-4"
+                  style={{ border: "1px solid rgba(255, 255, 255, 0.1)", width: "120px", height: "120px", background: "rgba(255, 255, 255, 0.07)"}}>
+                  <img src={ process.env.PUBLIC_URL + "./images/forgot-password-2.svg"} alt="logo" style={{ width: "76px", margin: "0 auto" }}/>
+                </div>
+                <div className="d-flex justify-content-center flex-column text-center">
+                  <h4 className="text-white" style={{ fontWeight: "700" }}>
+                  Create New Password
+                  </h4>
+                  <p className="my-2 col-lg-6 mx-auto" style={{ fontWeight: "400", color: "rgba(255, 255, 255, 0.7)",}}>
+                  Password must include at least combination of 8 characters, numbers and special character
+                  </p>
+                </div>
+                <div className="col-lg-6 mx-auto my-3 d-flex p-1">
+                  <div className="d-flex flex-column col">
+                    <div className="mx-2 mt-4">
+                      <Input type="password" icon="fa-solid fa-lock" style={inputStyle} placeholder="New Password" icon2="fa-regular fa-eye" icon3="fa-regular fa-eye-slash" bi bi-lock-fill id="password" name="password"/>
+                    </div>
+                    <div className="mx-2 my-4">
+                      <Input type="password" icon="fa-solid fa-lock" style={inputStyle} placeholder="Confirm Password" icon2="fa-regular fa-eye" icon3="fa-regular fa-eye-slash" bi bi-lock-fill id="password" name="password"/>
+                    </div>
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  className="btn mt-2 py-3 col-lg-6 d-flex mx-auto justify-content-center"
+                  style={{ borderRadius: "100px", background: "white", color: "black", fontWeight: "600", fontSize: "17px"}}
+                  onClick={() => setStep((i) => i + 1)}>
+                  Continue
+                </button>
+                </>
+              )
+            }
             <img src="./images/LOGO.svg" alt="logo" className="col-lg-2 position-absolute" style={{ top: "30px" }}/>
           </div>
         </div>
