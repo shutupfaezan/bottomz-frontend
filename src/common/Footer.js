a62d4cecb718e402ab304a3c5675422521c19849
shutup.faezan <faezanmakani13@gmail.com>
2023-09-30 22:44:53 +0530

    footer singular events responsive bits

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -4,19 +4,13 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <section
-      // className="pt-5 pb-4 d-flex flex-column position-relative w-100 justify-content-md-end text-black"
-      className="footer">
-      {/* <div className="d-flex flex-column flex-lg-row ml-3 mr-0"> */}
+    <section className="footer">
       <div className="footer-container">
-        {/* <div className="col-xs-12 d-md-flex justify-content-center mt-md-4 mb-4 mb-md-0 pt-2 col-md-3"> */}
         <div className="footer-left">
         <img src={`${window.location.origin}/images/LOGOBLACK.svg`} alt="Bottmzup logo" style={{ maxHeight: "50px" }} />
           <div className="footer-left-content line-left">
             <p>
-              Lorem ipsum dolor sit amet consectetur adipiscing elit ullamcorper
-              misem porttitor vel ut quis egestas mauris sapien ipsum diam odio
-              curabitur purus dolor sit amet consectur.
+              This is an evolving registered eventing and media company. We do nightlife better and smoother than everyone. We're still a developing company so any bugs or reposts are welcome. Just go to the Contact Us section and repost any bug you noticed and we'll surely get to the end of it.
             </p>
             <ul className="footer-socials">
               <li>
@@ -61,10 +55,8 @@ export default function Footer() {
             </ul>
           </div>
         </div>
-        {/* <div className="col-xs-12 p-0 py-md-5 col-lg-9 col-md-12 d-flex flex-column flex-md-row"> */}
         <div className="footer-right">
           <div className="footer-right-col">
-            {/* <h6 className="mb-md-3 mb-2" style={{ fontSize: "1rem" }}> */}
             <h6 style={{ fontSize: "1rem" }}>Explore</h6>
             <ul className="list-unstyled">
               <li>
@@ -106,7 +98,6 @@ export default function Footer() {
             </ul>
           </div>
           <div className="footer-right-col">
-            {/* <h6 className="mb-md-3 mb-2" style={{ fontSize: "1rem" }}> */}
             <h6 style={{ fontSize: "1rem" }}>Information</h6>
             <ul className="list-unstyled">
               <li>
@@ -148,7 +139,6 @@ export default function Footer() {
             </ul>
           </div>
           <div className="footer-right-col">
-            {/* <h6 className="mb-md-3 mb-2" style={{ fontSize: "1rem" }}> */}
             <h6 style={{ fontSize: "1rem" }}>Members</h6>
             <ul className="list-unstyled">
               <li>
@@ -192,14 +182,6 @@ export default function Footer() {
           </div>
         </div>
       </div>
-      {/* <hr className="mx-auto" style={{ width: "90%" }}></hr>
-      <a
-        className="mb-1 d-flex justify-content-end mr-4 mr-md-5 pr-lg-3"
-        href="mailto:info@bottmzup.com"
-        style={{ color: "black" }}
-      >
-        info@bottmzup.com
-      </a> */}
     </section>
   );
 }
