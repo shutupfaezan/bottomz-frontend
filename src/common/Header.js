db0d4527849e5d6d7836926d85fec8a9079d3232
shutup.faezan <faezanmakani13@gmail.com>
2023-11-23 01:38:18 +0530

    homepage header

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../css/Header.css"
-// import SearchBar from './SearchBar';
 import GlobalHeader from "./GlobalHeader";
 
 
@@ -9,13 +8,29 @@ export default function Header() {
   return (
     <>
     <GlobalHeader/>
-    <div className="position-relative">
-      <div className='svgbg MainHeader' style={{height: "430px"}}></div>
-      <div className='d-flex flex-column w-100 position-absolute' style={{top: "50%", left: "50%", transform: " translate(-50%, -50%)"}}>
-        <h1 className='d-flex justify-content-center align-self-center text-center mb-2 MainHeaderText primary-header' style={{fontWeight: "800", fontSize: "60px", color: "white"}}>Lets Make Live Happen</h1>
-        <p className='d-flex justify-content-center text-white align-self-center text-center mb-4 px-3' style={{fontWeight: "800", fontSize: "20px"}}>Shop millions of live events and discover can't-miss concerts, games, theater and more.</p> 
+    <div className="p-md-2">
+      <div className='position-relative px-lg-5 px-md-4 headerHeightClass' style={{background: "#0B0B0B"}}>
+        <div className="d-flex flex-column flex-md-row position-relative justify-content-between" style={{top: "140px"}}>
+          <div className="col-lg-6 mb-5">
+            <h1 className="HeaderGlobal pr-5">Immerse Yourself in the Energy of Events</h1>
+            <div className="px-3 mt-4 py-1" style={{borderLeft: "2px solid white", color: "white"}}>
+              <p className="mb-4 pb-1">Shop millions of live events and discover can't-miss concerts, games, theater and more.</p>
+              <div className="dropleft">
+                <p className="m-0 mr-md-4 ml-md-auto" style={{ fontSize: "19px", cursor: "pointer" }}>
+                  <small className="profile-login-btn rounded-pill" style={{ color: "black", background: "white", padding: "10px 30px", borderRadius: "20px" }}> Sign In</small>
+                  <small className="profile-login-btn rounded-pill ml-5" style={{ color: "white"}}>Sell <i class="fa-solid fa-arrow-right ml-3"></i></small>
+                </p>
+            </div>
+            </div>
+          </div>
+          <div className="col-lg-5">
+            <div className="h-100">
+              <img style={{width: '100%'}} src={process.env.PUBLIC_URL + "/images/HomeheaderImg.png"} alt=""/>
+            </div>
+          </div>
+        </div>
       </div>
-      </div>
+    </div>
     </>
   )
 }
